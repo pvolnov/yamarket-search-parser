6972e3bcb9546248d20d009fb519e72abd0c3f56
siunval <siunval91@gmail.com>
2021-02-09 23:49:06 +0500

    Updated parsed file

--- results_parse.py
+++ results_parse.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, MetaData, Table
 import json
+import csv
 
 engine = create_engine('sqlite:///dm.sqlite')
 metadata = MetaData(bind=engine)
@@ -9,15 +10,26 @@ searched = Table('searchresult', metadata, autoload=True)
 raw_data = searched.select().execute()
 data = [{x.keys()[i]: x[i] for i in range(len(x))} for x in raw_data]
 
-print(data[0].keys())
-k = set()
-names = set()
+key = set()
 for i in data:
-	#print(i)
-	names.add(i["name"])
-	for j in list(json.loads(i["specifications"].replace('\\\\','\\')).keys()):
-	    k.add(j)
+    # print(i["name"], i["url"])
+    for j in list(json.loads(i["specifications"].replace('\\\\', '\\')).keys()):
+        key.add(j)
 
-print(names)
-print()
-print(k)
+res = list()
+for i in data:
+    dct = dict()
+    if len(json.loads(i["specifications"].replace('\\\\', '\\')).values()) == 0:
+        continue
+    for j in sorted(key):
+        try:
+            dct.update({j: json.loads(i["specifications"].replace('\\\\', '\\'))[j]})
+        except KeyError:
+            continue
+    res.append(dct)
+
+with open("parsed_spec.csv", 'w') as f:
+    w = csv.DictWriter(f, sorted(key), dialect="excel")
+    w.writeheader()
+    for i in res:
+        w.writerow(i)
